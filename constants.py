7e7223a3436342e859104c9d0b75e4300e5d2e14
Roman Zots <v-romanzots@microsoft.com>
2024-09-28 01:18:39 +0300

    Fix comments

--- constants.py
+++ constants.py
@@ -1,5 +1,13 @@
+# Multipliers for total grade to reflect relevance of a type of work
 HW_MULTIPLIER = 0.1
 QUIZ_MULTIPLIER = 0.25
 EXAM_MULTIPLIER = 0.65
+
+# The number of quizzes
+QUIZ_QUANTITY = 2
+
+# The number of homeworks
+HW_QUANTITY = 3
+
 # The scale of quiz grades are 1 to 10, so grades should be multiplied by 10 to match other grades scale
 QUIZ_DATA_CONVERT_MULTIPLIER = 10
--- extract_grades.py
+++ extract_grades.py
@@ -1,25 +1,14 @@
-import numpy as np
-from constants import QUIZ_DATA_CONVERT_MULTIPLIER
+from constants import HW_QUANTITY, QUIZ_DATA_CONVERT_MULTIPLIER
 
 
 def extract_grades_hw_and_exams(hw_and_exams_data):
+    # Calculate averages for homework grades
+    homework_grades = [
+        int((int(row['Homework 1']) + int(row['Homework 2']) + int(row['Homework 3'])) / HW_QUANTITY)
+        for row in hw_and_exams_data]
+
     # Extract exam grades
     exam_grades = [int(student['Exam']) for student in hw_and_exams_data]
-
-    # Extract homework grades
-    homework_grade_lists = []
-
-    for row in hw_and_exams_data:
-        # Extract homework grades and append them to the list
-        homework_grades = [
-            int(row['Homework 1']),
-            int(row['Homework 2']),
-            int(row['Homework 3'])
-        ]
-        homework_grade_lists.append(homework_grades)
-
-    # Calculate averages for homeworks
-    homework_grades = np.sum(homework_grade_lists, axis=1) / len(homework_grade_lists[0])
 
     return homework_grades, exam_grades
 
