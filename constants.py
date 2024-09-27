59254fb59bd1edbf9da5f2c230b039b86cd6fae4
Roman Zots <v-romanzots@microsoft.com>
2024-09-27 23:50:00 +0300

    Initial commit

---
+++ constants.py
@@ -0,0 +1,5 @@
+HW_MULTIPLIER = 0.1
+QUIZ_MULTIPLIER = 0.25
+EXAM_MULTIPLIER = 0.65
+# The scale of quiz grades are 1 to 10, so grades should be multiplied by 10 to match other grades scale
+QUIZ_DATA_CONVERT_MULTIPLIER = 10
