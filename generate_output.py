41b8ed49fd25478c6383e6c17f2927eeb821a467
Roman Zots <v-romanzots@microsoft.com>
2024-09-28 00:17:49 +0300

    Add and Update comments

--- generate_output.py
+++ generate_output.py
@@ -8,13 +8,17 @@ os.makedirs(output_dir, exist_ok=True)
 def generate_output(students, grades_total):
     # Create a DataFrame for students
     students_df = pd.DataFrame(students)
+
+    # Sort by NetID in order to match the order of calculated grades list
     students_sorted_by_net_id = students_df.sort_values(by='NetID')
 
+    # Add the calculated grades as 'Final Grades' column
     students_sorted_by_net_id['Final Grades'] = grades_total
 
+    # Sort by grades
     students_sorted_by_grades = students_sorted_by_net_id.sort_values(by='Final Grades', ascending=False)
 
-    # Group the DataFrame by Group
+    # Group the DataFrame by 'Group' column
     students_grouped = students_sorted_by_grades.groupby('Group')
 
     # Generate resulting sheets
